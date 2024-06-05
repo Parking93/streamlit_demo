57f6a1db34da7e538b2457e76ff4c369ec0eab75
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2024-06-05 15:53:33 +0900

    Update demopage1.py

--- demopage1.py
+++ demopage1.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import os
-# st.write(os.getcwd())
+st.write(os.getcwd())
 # st.set_page_config(initial_sidebar_state="collapsed")
 
 st.page_link("demopage.py", label="Home", icon="🏠")
 
-st.page_link("pages/untitled1.py", label="untitled1", icon="1️⃣")
+# st.page_link("pages/untitled1.py", label="untitled1", icon="1️⃣")
 
 # st.page_link("pages/stockdashboard.py", label="stockdashboard", icon="📈")
 
